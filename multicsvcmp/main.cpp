973e64922a3eaf0ec9349a7b537cb0d314a3c9af
Charles Kerr <punt1959@users.noreply.github.com>
2022-11-21 07:27:29 -0500

    Initial Commit

---
+++ multicsvcmp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  multicsvcmp
+//
+//  Created by Charles Kerr on 11/21/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
